fab2995e6c307a9ec9d9e64ae873291ea2eaecac
rolex9799 <rolandsrepetto3@inbox.lv>
2020-12-22 23:07:24 +0200

    commit129

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-const toggleButton = document.getElementsByClassName('toggle-button')[0]
-  const navbarLinks = document.getElementsByClassName('navbar-links')[0]
+// const toggleButton = document.getElementsByClassName('toggle-button')[0]
+//   const navbarLinks = document.getElementsByClassName('navbar-links')[0]
 
-  toggleButton.addEventListener('click', () => {
-      navbarLinks.classList.toggle('active')
-  })
+//   toggleButton.addEventListener('click', () => {
+//       navbarLinks.classList.toggle('active')
+//   })
 
   mybutton = document.getElementById("myBtn");
       
@@ -22,3 +22,39 @@ const toggleButton = document.getElementsByClassName('toggle-button')[0]
       document.documentElement.scrollTop = 0; 
     }
 
+
+    const navSlide = () => {
+      const burger = document.querySelector('.burger');
+      const nav = document.querySelector('.nav-links')
+    
+      const navLinks = document.querySelectorAll('.nav-links li');
+    
+      burger.addEventListener('click', ()=>{
+    
+        //Toggle Nav
+        nav.classList.toggle('nav-active');
+    
+    
+         //Animate links
+        navLinks.forEach((link, index) =>{
+    
+          if(link.style.animation){
+            link.style.animation = ''
+          }else{
+            link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 1.1}s`;
+          }
+            
+      
+            
+        });
+        //Burger Animation
+    
+        burger.classList.toggle('toggle');
+      });
+    
+     
+    
+      
+    }
+    
+    navSlide();
